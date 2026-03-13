c18af06fe02f71dfdcc9cacc1641f59ab9b7dac8
Vercel Deployer <test@example.com>
2026-03-13 00:36:22 +0000

    Add custom BK favicon

---
+++ app/icon.tsx
@@ -0,0 +1,40 @@
+import { ImageResponse } from 'next/og'
+
+// Route segment config
+export const runtime = 'edge'
+
+// Image metadata
+export const size = {
+  width: 32,
+  height: 32,
+}
+export const contentType = 'image/png'
+
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 18,
+          background: '#2563EB', // Tailwind blue-600
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
+          borderRadius: 8,
+          fontWeight: 900,
+        }}
+      >
+        BK
+      </div>
+    ),
+    // ImageResponse options
+    {
+      ...size,
+    }
+  )
+}
